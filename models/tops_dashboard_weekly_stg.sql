220ce6096b6a70bc9ef3699531abf7e76e976b04
MastanBandi <134686858+MastanBandi@users.noreply.github.com>
2023-08-03 16:48:57 +0530

    Update tops_dashboard_weekly_stg.sql

--- models/tops_dashboard_weekly_stg.sql
+++ models/tops_dashboard_weekly_stg.sql
@@ -1,3 +1,12 @@
+{{ config(
+    materialized='incremental',
+    partition_by={
+          "field": "year",
+          "data_type": "timestamp",
+          "granularity": "day"
+        }
+) }}
+	
 select
 	name,
 	branch,
@@ -5,3 +14,7 @@ select
 	year
 from {{ ref('testing_raw_temp') }} temp
 where temp.failure_reason is null
+
+{% if is_incremental() %}
+    and temp.year > ( select COALESCE(max(year), TIMESTAMP('2000-01-01 00:00:00.0000')) from {{ this }} )
+{% endif %}
